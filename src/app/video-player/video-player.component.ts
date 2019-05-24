7c3a564c1e6254d1cc45a9e23e563b2a8ee5c25a
codevipin <vipinverma@live.com>
2019-05-24 06:57:01 -0400

    setup/fetch id from query param

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-video-player',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoPlayerComponent implements OnInit {
 
-  constructor() { }
+  id:string = '4d79041e-f25f-421d-9e5f-3462459b9934';
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.router.navigate([''], { queryParams: { id:this.id } });
+    // Although the id is hard-coded in this case and we could directly fetch the data,
+    // but normally in our we have to get it from the query params,thence I am doing this
+    this.route.queryParams
+    .subscribe(params => {
+      
+      //fetch the details for the particular id
+    })
   }
 
 }
