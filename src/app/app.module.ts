35e1447db2e4731a4502348d00d03a4a4ee9342a
codevipin <vipinverma@live.com>
2019-05-24 05:39:04 -0400

    added footer component to display chorus brand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { TranscriptComponent } from './video-player/transcript/transcript.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoPlayerComponent,
-    TranscriptComponent
+    TranscriptComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
